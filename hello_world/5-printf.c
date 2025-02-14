33f1226a97c2f422aaeac5515a64a39e4721eece
AngelD89 <angeldeprey@yahoo.com>
2025-02-14 12:48:15 -0400

    5print

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *              followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+ # include <stdio.h>
+ 
+int main(void)
+	{
+		printfiprintf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
