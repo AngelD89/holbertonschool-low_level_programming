07e74d90ae07125c0aaa95ba6a8f1c74c2e4a361
AngelD89 <angeldeprey@yahoo.com>
2025-02-27 11:12:10 -0400

    0reset

---
+++ 0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Updates the value of the integer it points to 98
+ * @n: Pointer to an int
+ *
+ * Return: void 9nothing0
+ */
+void reste_to_98(int *n)
+{
+	*n = 98;
+}
