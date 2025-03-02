a5d1a7cdafe0acd5224bc1cfa7f5fc7e85d5fddc
AngelD89 <angeldeprey@yahoo.com>
2025-03-02 17:27:36 -0400

    6cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,27 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to modify.
+ * cap_string - capitalizes all words in a string
+ * @str: pointer to the string to capitalize
  *
- * Return: A pointer to the modified string.
+ * Return: pointer to the modified string
  */
 char *cap_string(char *str)
 {
 	int i = 0, j;
-	char sep[] = "\t\n,;.!?\"(){}";
-
-	if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] -= 32;
+	char sep[] = " \t\n,;.!?\"(){}";
 
 	while (str[i])
 	{
-		for (j = 0; sep[j]; j++)
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+
+		for (j = 0; sep[j] != '\0'; j++)
 		{
-			if (str[i] == sep[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			{
+			if (str[i] == sep[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
 				str[i + 1] -= 32;
-				break;
-			}
 		}
+
 		i++;
 	}
 
