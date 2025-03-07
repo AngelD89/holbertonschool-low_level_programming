d5e49b670ca412efd755a1beb8620ce5f1c0c3b6
AngelD89 <angeldeprey@yahoo.com>
2025-03-07 10:21:32 -0400

    7print

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -13,10 +13,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			for (j = 0; j < 8; j++)
-			{
-				putchar(a[i][j]);
-			}
-			putchar('\n');
+			putchar(a[i][j]);
 		}
+		putchar('\n');
 	}
+}
