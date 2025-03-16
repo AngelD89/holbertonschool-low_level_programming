532c90ff86593d6897e2b7080ec933fe0a71c72f
AngelD89 <angeldeprey@yahoo.com>
2025-03-16 08:45:04 -0400

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a new struct for a dog
+ * @name: Name of the dog (char pointer)
+ * @age: Age of the dog (float)
+ * @owner: Owner of the dog (char pointer)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
