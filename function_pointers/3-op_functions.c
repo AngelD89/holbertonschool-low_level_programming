0df8c2cf61a1c01ea4847b42a1f815d60473ed55
AngelD89 <angeldeprey@yahoo.com>
2025-03-23 18:24:07 -0400

    3opfunctions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,24 +1,49 @@
 #include "3-calc.h"
 
-/* Addition */
+/**
+ * op_add - Adds two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The sum of a and b.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
-/* Substraction */
+/**
+ * op_sub - Subtracts the second integer from the first.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The difference of a and b.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
-/* Multiplication */
+/**
+ * op_mul - Multiplies two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The product of a and b.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
-/* Division */
+/**
+ * op_div - Divides the first integer by the second.
+ * @a: First integer (numerator).
+ * @b: Second integer (denominator).
+ *
+ * Return: The result of the division (a / b).
+ *         If the denominator is 0, prints "Error" and exits with status 100.
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -29,7 +54,14 @@ int op_div(int a, int b)
 	return (a / b);
 }
 
-/* Modulo */
+/**
+ * op_mod - Computes the remainder of the division of two integers.
+ * @a: First integer (numerator).
+ * @b: Second integer (denominator).
+ *
+ * Return: The remainder of the division (a % b).
+ *         If the denominator is 0, prints "Error" and exits with status 100.
+ */
 int op_mod(int a, int b)
 {
 	if (b == 0)
