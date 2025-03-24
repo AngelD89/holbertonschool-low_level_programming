27fffe0a78ef6aba178ca468db698df51789205c
AngelD89 <angeldeprey@yahoo.com>
2025-03-23 22:01:59 -0400

    0sum

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all its parameters.
+ * @n: The number of parameters to be summed.
+ *
+ * Return: The resulting sum or 0 if n == 0.
+ */
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
