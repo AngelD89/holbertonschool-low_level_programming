71cf0d291fa8918e26a91fef7764853e1e8a8e8c
AngelD89 <angeldeprey@yahoo.com>
2025-03-23 23:10:08 -0400

    1print

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -10,5 +10,6 @@
  * Return: The resulting sum or 0 if n == 0.
  */
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
