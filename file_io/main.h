8f11ed21c3b7632c8f3f889d85db9f34f0115292
AngelD89 <angeldeprey@yahoo.com>
2025-04-08 09:47:44 -0400

    0readtext

--- file_io/main.h
+++ file_io/main.h
@@ -11,15 +11,6 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- * error_exit - Handles error messages and exits with a specific code.
- * @exit_code: The exit code.
- * @message: The error message to print.
- * @file_name: The file name to include in the error message.
- */
-
-
-int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
